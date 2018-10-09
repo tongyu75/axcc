8dec3426381009f3609aef38b350239ca428c52a
tongyu75 <280005464@qq.com>
2018-10-10 02:41:48 +0800

    取消二级代理，数据库结构变化

--- src/main/resources/file/axcc.sql
+++ src/main/resources/file/axcc.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50723
 File Encoding         : 65001
 
-Date: 2018-10-09 23:00:55
+Date: 2018-10-10 02:39:26
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -22,7 +22,6 @@ DROP TABLE IF EXISTS `agent_share`;
 CREATE TABLE `agent_share` (
   `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
   `user_id` int(10) NOT NULL COMMENT '会员ID号',
-  `parent_id` int(10) NOT NULL COMMENT '父级ID号',
   `agent_money` float(10,2) NOT NULL COMMENT '代理员当月业绩',
   `create_date` date NOT NULL COMMENT '月份时间',
   PRIMARY KEY (`id`)
@@ -33,15 +32,15 @@ CREATE TABLE `agent_share` (
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for `bussiness`
+-- Table structure for `business`
 -- ----------------------------
-DROP TABLE IF EXISTS `bussiness`;
-CREATE TABLE `bussiness` (
+DROP TABLE IF EXISTS `business`;
+CREATE TABLE `business` (
   `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
   `user_id` int(10) NOT NULL COMMENT '普通会员ID号',
   `agent_id` int(10) DEFAULT NULL COMMENT '代理员ID号',
   `admin_id` int(10) DEFAULT NULL COMMENT '管理员ID号',
-  `wait_num` int(10) DEFAULT NULL,
+  `wait_num` int(10) DEFAULT NULL COMMENT '排位',
   `buy_money` float(10,2) DEFAULT NULL COMMENT '购车费用',
   `buy_type` int(2) NOT NULL COMMENT '购车类型（1：10万；2:20万；3:30万；5:50万）',
   `buy_status` int(2) DEFAULT NULL COMMENT '会员购车状态（0：未排队，1：排队中，2：已出车）',
@@ -53,7 +52,7 @@ CREATE TABLE `bussiness` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='代理员业务表\r\n\r\n1> 没有40万的车型\r\n2>会员申请排队购车，管理员审核通过后，会员到代理员缴纳购车所需的费用，\r\n    代理员通过会员手机号查看会员提交的购车申请是否已被管理员审核通过，\r\n    若已通过，可办理业务收取会员的购车费用，并记录到本表中，\r\n    返回“缴费成功”状态，管理员进行排号。\r\n3>由于会员可以到任意代理员处支付费用（不需要指定某个具体的代理员），\r\n    所以代理员需要通过会员手机号查询该会员是否已提交了购车申请，管理员是否已审核通过，\r\n    然后办理业务，支付完成后向管理员反馈支付成功的状态，最后由管理员进行排号';
 
 -- ----------------------------
--- Records of bussiness
+-- Records of business
 -- ----------------------------
 
 -- ----------------------------
@@ -67,8 +66,7 @@ CREATE TABLE `history` (
   `buy_money` float(10,2) NOT NULL COMMENT '会员购车费用',
   `buy_type` int(2) NOT NULL COMMENT '会员购车类型（1：10万，2:20万，3:30万，5:50万）',
   `buy_status` int(2) NOT NULL COMMENT '会员购车状态（0：未出车；1：已出车，2：已取消）',
-  `agent_ag_money` float(10,2) DEFAULT NULL COMMENT '代理员当月获取的二级代理业绩的提成',
-  `agent_me_money` float(10,2) DEFAULT NULL COMMENT '代理员当月获取的会员现场签单业务提成',
+  `agent_me_money` float(10,2) DEFAULT NULL COMMENT '代理员当月获取的会员现场签单业务提成的3%',
   `create_time` datetime DEFAULT NULL COMMENT '时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='历史记录表\r\n\r\n1> 记录所有会员的购车记录，代理员每月业绩。';
@@ -130,8 +128,8 @@ CREATE TABLE `users` (
   `image` varchar(100) DEFAULT NULL COMMENT '用户头像',
   `bank_card` varchar(50) DEFAULT '未认证' COMMENT '银行卡号',
   `bank_addr` varchar(100) DEFAULT '未认证' COMMENT '开户行地址',
-  `parent_id` varchar(16) DEFAULT NULL COMMENT '推荐人ID/上级代理ID',
-  `original` varchar(200) DEFAULT NULL COMMENT '所有直接或间接相关的上级，逗号隔开（1,2,3,4...）',
+  `parent_id` int(10) DEFAULT NULL COMMENT '推荐人ID',
+  `original` varchar(200) DEFAULT NULL COMMENT '会员所有直接或间接相关的上级推荐人ID号，逗号隔开（1,2,3,4...）',
   `user_role` int(2) NOT NULL DEFAULT '2' COMMENT '用户角色（0：管理员，1：代理员，2：普通会员）',
   `create_time` datetime NOT NULL COMMENT '注册时间',
   `update_time` datetime DEFAULT NULL COMMENT '修改时间',
