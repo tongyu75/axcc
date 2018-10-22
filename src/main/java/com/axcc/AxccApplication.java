3e6174c628b51c64e1844e0bd43fe247774c4d7c
tongyu75 <280005464@qq.com>
2018-10-22 23:37:53 +0800

    修改跨域过滤器不起作用的问题

--- src/main/java/com/axcc/AxccApplication.java
+++ src/main/java/com/axcc/AxccApplication.java
@@ -3,11 +3,14 @@ package com.axcc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
 @SpringBootApplication
 // 将项目中对应的mapper类的路径加进来就可以了
 @MapperScan("com.axcc.dao")
+@ServletComponentScan
 public class AxccApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(AxccApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AxccApplication.class, args);
+    }
 }
