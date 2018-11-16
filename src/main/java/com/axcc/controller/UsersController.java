adcc4d757b59a9cebbbad08813c52485cac72bde
tongyu75 <280005464@qq.com>
2018-11-17 00:58:42 +0800

    更新

--- src/main/java/com/axcc/controller/UsersController.java
+++ src/main/java/com/axcc/controller/UsersController.java
@@ -669,7 +669,7 @@ public class UsersController {
      */
     @RequestMapping(value="/listWait",method = RequestMethod.POST)
     public Map<String,Object> listWait(
-            @RequestParam(value = "phone", required = false) String phone,
+            @RequestParam(value = "loginName", required = false) String loginName,
             @RequestParam(value = "buyStatus", required = false) Integer buyStatus,
             @RequestParam(value = "pageNum", required = true) Integer pageNum,
             @RequestParam(value = "pageSize", required = true) Integer pageSize){
@@ -678,8 +678,8 @@ public class UsersController {
         Map<String,Object> result = new HashMap<String, Object>();
         BusinessUser bean = new BusinessUser();
         bean.setBuyStatus(buyStatus);
-        if(!"".equals(phone)){
-            bean.setLoginName(phone);
+        if(!"".equals(loginName)){
+            bean.setLoginName(loginName);
         }
         // 查询记录总条数
         int count = businessService.countBusinessUserByBean(bean);
