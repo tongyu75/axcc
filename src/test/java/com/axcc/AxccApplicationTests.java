c729d99cd3cdf369ced06fa6c6004031e7502e02
tongyu75 <280005464@qq.com>
2019-01-07 14:03:20 +0800

    针对开发、测试、生产环境，通过配置文件来确定环境内容，即在执行的时候指定具体环境如nuhup java -jar  lqyspringboot-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod &

--- src/test/java/com/axcc/AxccApplicationTests.java
+++ src/test/java/com/axcc/AxccApplicationTests.java
@@ -1,20 +1,34 @@
 package com.axcc;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = AxccApplicationTests.class)
+@WebAppConfiguration
 public class AxccApplicationTests {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	@Test
-	public void contextLoads() {
-		//System.out.print(1111);
-		//logger.info("12344");
+	@Before
+	public void init() {
+		System.out.println("开始测试-----------------");
 	}
+
+	@Test
+	public void test() {
+		System.out.println("测试啦-----------------");
+	}
+
+	@After
+	public void after() {
+		System.out.println("测试结束-----------------");
+	}
+
 
 }
