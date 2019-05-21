99b704c6e4bec40e9a05d50e5c0d639fdafaf967
tongshr <tongshr@yonyou.com>
2019-05-21 10:38:30 +0800

    1、添加tomcat启动 2、pom中添加<filtering>true</filtering>

--- src/main/java/com/axcc/AxccApplication.java
+++ src/main/java/com/axcc/AxccApplication.java
@@ -3,7 +3,9 @@ package com.axcc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -11,8 +13,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @MapperScan("com.axcc.dao")
 @ServletComponentScan
 @EnableScheduling
-public class AxccApplication {
+public class AxccApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(AxccApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AxccApplication.class);
     }
 }
